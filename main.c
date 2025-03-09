8fb0f8354cf7a90a9327f22eaf5565f2d6c64306
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-03-08 19:14:52 -0500

    added a REPL loop

--- main.c
+++ main.c
@@ -3,23 +3,22 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
   setbuf(stdout, NULL);
-
-  // Uncomment this block to pass the first stages
-  printf("$ ");
 
   // Wait for user input
   char input[100];
 
-  fgets(input, 100, stdin);
+  while(1) {
+    printf("$ ");
+    fgets(input, 100, stdin);
 
-  size_t len = strlen(input);
-  if(len > 0 && input[len - 1] == '\n') {
-    input[len - 1] = '\0';
+    size_t len = strlen(input);
+    if(len > 0 && input[len - 1] == '\n') {
+      input[len - 1] = '\0';
+    }
+
+    printf("%s: command not found \n", input);
   }
-
-  printf("%s: command not found", input);
 
   return 0;
 }
