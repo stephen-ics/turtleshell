d5553a1645ff9a526c4609d48a400a8e8c544204
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-03-08 19:15:29 -0500

    added the exit function

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
+  // Flush after every printf
   setbuf(stdout, NULL);
+
+  // Uncomment this block to pass the first stages
 
   // Wait for user input
   char input[100];
@@ -15,6 +18,10 @@ int main(int argc, char *argv[]) {
     size_t len = strlen(input);
     if(len > 0 && input[len - 1] == '\n') {
       input[len - 1] = '\0';
+    }
+
+    if(strcmp("exit 0", input)) {
+      break;
     }
 
     printf("%s: command not found \n", input);
